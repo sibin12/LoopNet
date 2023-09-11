1019ed9198faec912548b6bcc910f3fab533bd58
Sibin Silu <sibinsilu001@gmail.com>
2023-09-11 21:36:46 +0530

    nested comment done

--- server/routes/comment.js
+++ server/routes/comment.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { addComment, deleteComment, getComments } from "../controllers/comment.js";
+import { addComment, deleteComment, getComments, replyComment, deleteReply} from "../controllers/comment.js";
 import {verifyToken} from "../middlewares/verifyToken.js"
 const router = express.Router();
 
 router.post("/", verifyToken, addComment)
+router.post("/reply", verifyToken, replyComment)
+router.delete("/:commentId/reply/:replyId", verifyToken, deleteReply)
 router.delete("/:id", verifyToken, deleteComment)
 router.get("/:videoId", getComments)
 
